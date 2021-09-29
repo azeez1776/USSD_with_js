e69d117a4af049f22d6f29cf84d22d67fab9c8e4
azeez1776 <aboud3am81@gmail.com>
2021-09-29 20:18:50 +0300

    added http requests for the USSD menu

--- server.js
+++ server.js
@@ -10,6 +10,36 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.get('/', (req, res) => {
+    res.send("USSD with js");
+})
+
+app.post('/', (req, res) => {
+    let { sessionId, serviceCode, phoneNumber, text } = req.body;
+
+    if (text === '') {
+
+        let response = `CON get free unlimited internet for a week
+        1.YES
+        2.NO
+        `
+        res.send(response);
+    }
+
+    else if (text === '1') {
+
+        let response = `Kijana pambana hamna cha bure`
+
+        res.send(response);
+    }
+
+    else {
+
+        res.status(400).send('bad request');
+    }
+
+})
+
 app.listen(port => {
     console.log(`Listening to port ${port}`)
 })
